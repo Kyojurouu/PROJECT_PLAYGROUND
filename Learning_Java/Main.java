4df73aaff3e399d581a7ac296bb177be14f1b7ff
John Mikael <147248782+Kyojurouu@users.noreply.github.com>
2025-08-21 18:50:24 +0800

    HTML-JAVA Updates

---
+++ Learning_Java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Been a while since I coded in Java Language");
+        System.out.println("This is just a practice or simply a refresher");
+        System.out.println("Let me add the date as well, 8/21/2025, so I could reminisce it in the future");
+    }
+}
